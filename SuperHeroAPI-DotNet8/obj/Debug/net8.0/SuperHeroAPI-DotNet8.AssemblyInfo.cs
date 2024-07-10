9a6416d719569b412c1c2cbb36b9af5485ff3cb1
Karan <karanbir97@outlook.com>
2024-07-09 22:51:31 -0500

    Just testing

--- SuperHeroAPI-DotNet8/obj/Debug/net8.0/SuperHeroAPI-DotNet8.AssemblyInfo.cs
+++ SuperHeroAPI-DotNet8/obj/Debug/net8.0/SuperHeroAPI-DotNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccbb8c1ce15f81587b8195716e5866874e05c0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
