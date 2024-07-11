4d0aaa798c2b63d808bd2f44f513593b24ad6380
Karan <karanbir97@outlook.com>
2024-07-10 20:58:39 -0500

    Added the Entity

---
+++ SuperHeroAPI-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public GetHero(int id)
+        {
+            if (id == 
+            return hero;
+        }
+    }
+}
