03761ed128c16c80277056ec5ca330cb5cb2b44e
Karan <karanbir97@outlook.com>
2024-09-10 21:46:57 -0500

    First commit

---
+++ SuperHeroApi-DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+    }
+}
