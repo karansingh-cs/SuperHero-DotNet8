d33a7fa03f9b56b397a37b0dcca4c3a94708e2e9
karansingh-cs <karanbir97@outlook.com>
2024-12-14 23:10:50 -0600

    Added Data Context and Migration

--- SuperHeroApi-DotNet8/Program.cs
+++ SuperHeroApi-DotNet8/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SuperHeroApi_DotNet8.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});   
 
 var app = builder.Build();
 
--- SuperHeroApi-DotNet8/obj/Debug/net8.0/SuperHeroApi-DotNet8.AssemblyInfo.cs
+++ SuperHeroApi-DotNet8/obj/Debug/net8.0/SuperHeroApi-DotNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperHeroApi-DotNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d33dbb581a09c23d85c3b7fce797778636b34da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbd392674c1a1059a06030085749ac23a99f022")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperHeroApi-DotNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperHeroApi-DotNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
