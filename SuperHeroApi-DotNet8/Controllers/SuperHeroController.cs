ea4bbdf5bcbfe024ccbab821cff85b810a215c04
karansingh-cs <karanbir97@outlook.com>
2024-12-23 23:24:44 -0600

    Added the HttpPost method

--- SuperHeroApi-DotNet8/Controllers/SuperHeroController.cs
+++ SuperHeroApi-DotNet8/Controllers/SuperHeroController.cs
@@ -27,14 +27,23 @@ namespace SuperHeroApi_DotNet8.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
         {
             var hero = await _context.SuperHeroes.FindAsync(id);
-            if (hero == null)
+            if (hero is null)
                 return NotFound("Hero Not Found");
 
             return Ok(hero);
 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetAllHeroes());
         }
     }
 }
